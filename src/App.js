be7d1f3b5b6825992d21f292d85629092fd121e7
Kelly <renkelei.kelly@gmail.com>
2020-12-26 17:41:41 +0200

    updating APP

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import './App.css';
+import React, { createContext, useEffect } from "react";
+import ReactDom from "react-dom";
+
+import "./App.css";
+import storeInstance from "./store/Store";
+import SearchForm from "./components/SearchForm";
+import SearchResultDef from "./components/SearchResultDef";
+
+export const StoreContext = createContext();
 
 function App() {
+
   return (
-    <div className="App">
-      Kelly's dictionary app
-    </div>
+    <StoreContext.Provider value={storeInstance}>
+      <div className="App">Kelly's dictionary app</div>
+      <SearchForm />
+      <SearchResultDef />
+    </StoreContext.Provider>
   );
 }
 
