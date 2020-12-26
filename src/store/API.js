b804ef5329e62cfe22cd1dbd27ecfa20ded39140
Kelly <renkelei.kelly@gmail.com>
2020-12-26 16:37:44 +0200

    set up api

---
+++ src/store/API.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+import config from "../config";
+
+
+export const wordsArray = require("an-array-of-english-words");
+
+export const getDefByWord = (word) => {
+  return axios
+    .request({
+      method: "GET",
+      url: config.api.URL,
+      params: { term: word },
+      headers: {
+        "x-rapidapi-key": config.api.KEY,
+        "x-rapidapi-host": config.api.HOST,
+      },
+    })
+    .then(function (response) {
+      console.log(response.data);
+      return response.data;
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+};
