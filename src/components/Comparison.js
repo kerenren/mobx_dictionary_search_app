da8b1f430d8ed6ea59db864bbf5e8b7af6afc61f
Kelly <renkelei.kelly@gmail.com>
2020-12-27 14:08:39 +0200

    adding conditonal rendering for comparison results

--- src/components/Comparison.js
+++ src/components/Comparison.js
@@ -5,20 +5,32 @@ import { StoreContext } from "../App";
 
 const Comparison = observer(() => {
   const store = useContext(StoreContext);
+  const letter = () => {
+    return <span>{store.currentWord}</span>;
+  };
 
   return (
     <div>
-      <h1>Stats</h1>
-      <p>Words that start with the letter: {store.startLetterCount()} </p>
-      <p>Words that end with the letter: {store.endLetterCount()}</p>
-      <p>
-        Times that the letter appears in dictionary:
-        {store.timesIncludedCount()}
-      </p>
-      <p>
-        Words that have the same letter repeated in conjunction:{" "}
-        {store.repeatedLetterCount()}
-      </p>
+      {store.currentWord && (
+        <>
+          {" "}
+          <h1>Stats</h1>
+          <p>
+            Words that start with "{letter()}" : {store.startLetterCount()}{" "}
+          </p>
+          <p>
+            Words that end with "{letter()}" : {store.endLetterCount()}
+          </p>
+          <p>
+            Times that "{letter()}" appears in dictionary:
+            {store.timesIncludedCount()}
+          </p>
+          <p>
+            Words that have the same letter repeated in conjunction:
+            {store.repeatedLetterCount()}
+          </p>{" "}
+        </>
+      )}
     </div>
   );
 });
