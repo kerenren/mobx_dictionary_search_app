49a9dc86c532d0de509d4b7f9bae9bac70d06114
Kelly <renkelei.kelly@gmail.com>
2020-12-27 22:21:32 +0200

    creating search result title component

---
+++ src/components/SearchResultTitle.js
@@ -0,0 +1,22 @@
+/** @jsxImportSource @emotion/react */
+import React from "react";
+
+
+
+const SearchResultTitle = ({ title }) => {
+  return (
+    <h2
+      css={{
+        textTransform: "uppercase",
+        color: "#4183f2",
+        fontWeight: 500,
+        letterSpacing: "0.5px",
+        lineHeight: "16px",
+      }}
+    >
+      {title}
+    </h2>
+  );
+};
+
+export default SearchResultTitle
