521a55f2c787c69dc30ad7c9c11ff1deab53cb11
Kelly <renkelei.kelly@gmail.com>
2020-12-27 19:59:57 +0200

    add syle for app. moved media query variable to store folder; customized side bar style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 /** @jsxImportSource @emotion/react */
 
 import React, { createContext } from "react";
-import { jsx } from "@emotion/react";
-import facepaint from "facepaint";
 
-import "./App.css";
+import "./styles/custom.css";
+import { mq } from "./store/mediaQuery";
 import Header from "./components/Header";
-
 import storeInstance from "./store/Store";
 import Comparison from "./components/Comparison";
 import SearchResultDef from "./components/SearchResultDef";
@@ -14,29 +12,53 @@ import SearchResultDef from "./components/SearchResultDef";
 export const StoreContext = createContext();
 
 function App() {
-  const breakpoints = [576, 768, 992, 1200];
-  const mq = facepaint(breakpoints.map((bp) => `@media (min-width: ${bp}px)`));
   return (
     <StoreContext.Provider value={storeInstance}>
       <div
         className="App"
         css={mq({
-          color: ["green", "gray", "hotpink"],
+          color: ["#FCA311", "#FCA311", "#14213D"],
           fontFamily: "Roboto",
           textAlign: "center",
           display: "flex",
           flexDirection: "column",
-          justifyContent: "space-between",
+          justifyContent: "center",
           alignItems: "center",
           boxSizing: "border-box",
           width: "100vw",
           height: "100vh",
-          padding: ["0 10px", "040px", "050px", "0 60px"],
+          padding: ["0 10px", "0 40px", "0 50px", "0 60px"],
+          fontSize: "16px",
+          margin: "auto",
+          backgroundColor: "#f7f7f7",
         })}
       >
         <Header />
-        <Comparison />
-        <SearchResultDef />
+        <div
+          css={mq({
+            width: ["90%", "80%"],
+            maxWidth: "100%",
+            minHeight: "30rem",
+            margin: "100px auto 100px auto",
+            display: "flex",
+            justifyContent: ["flex-start", "flex-start", "center"],
+            alignItems: "center",
+            flexDirection: ["column", "column", "row"],
+            boxShadow: "0 2px 2px 0 rgba(0,0,0,0.12)",
+            padding: ["24px", "32px"],
+            textAlign: "left",
+            borderRadius: "6px",
+            backgroundColor: "white",
+            overflowY: "auto",
+            "&:hover": {
+              boxShadow:
+                "0 8px 10px 1px rgba(0,0,0,0.07), 0 3px 14px 3px rgba(0,0,0,0.06), 0 4px 5px 0 rgba(0,0,0,0.1);",
+            },
+          })}
+        >
+          <Comparison />
+          <SearchResultDef />
+        </div>
       </div>
     </StoreContext.Provider>
   );
