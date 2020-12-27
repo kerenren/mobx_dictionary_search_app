496ceab0acb785ac6f60e00e2c63a56ba10d9f0a
Kelly <renkelei.kelly@gmail.com>
2020-12-27 16:53:42 +0200

    implemented responsive design for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,21 +4,47 @@ import React, { createContext } from "react";
 import { jsx } from "@emotion/react";
 
 import SearchForm from "./SearchForm";
+import { mq } from "../styles/global";
 
 const Header = () => {
   return (
     <header
-      css={{
+      css={mq({
         background:
           "linear-gradient(90deg, rgba(26,40,83,1) 0%, rgba(42,47,97,1) 48%, rgba(20,33,61,1) 84%)",
         width: "100vw",
-        position: "fixed",
+        position: "sticky",
         top: 0,
-      }}
+        zIndex: 100,
+        color: "white",
+        display: "flex",
+        flexDirection: ["column", "row"],
+        alignItems: "center",
+        height: "200px",
+        textAlign: "left",
+        padding: "20px 0",
+      })}
     >
-      <h1>Kelly's dictionary app</h1>
-      <h2>Make your words meaningful</h2>
-      <SearchForm />
+      <div css={{ flexGrow: 1, padding: "0 20px" }}>
+        <h1
+          css={{
+            borderBottom: "1px solid #FCA311",
+            paddingBottom: "10px",
+          }}
+        >
+          Kelly's dictionary app
+        </h1>
+        <span css={{ fontsize: "16px" }}>Make your words meaningful</span>
+      </div>
+      <div
+        css={mq({
+          flexGrow: 2,
+          width: ["90%", "65%"],
+          padding: [["20px 0 0 0"], "0 20px 0 0"],
+        })}
+      >
+        <SearchForm />
+      </div>
     </header>
   );
 };
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,5 +1,9 @@
+/** @jsxImportSource @emotion/react */
+
 import React from "react";
 import { StoreContext } from "../App";
+
+import { jsx } from "@emotion/react";
 
 const SearchForm = () => {
   let searchTerm;
@@ -12,20 +16,55 @@ const SearchForm = () => {
   return (
     <StoreContext.Consumer>
       {(store) => (
-        <form onSubmit={(e) => handleSubmit(e, store)}>
-          <label>
-            Search the word:
-            <input
-              type="text"
-              id={"searchTerm"}
-              className="form-control"
-              placeholder={"Search a word ..."}
-              ref={(node) => {
-                searchTerm = node;
-              }}
-            />
-          </label>
-          <input type="submit" value="Submit" />
+        <form
+          css={{
+            display: "flex",
+            alignItems: "center",
+            margin: "0 auto",
+            justifyContent: "center",
+            fontSize: "16px",
+          }}
+          onSubmit={(e) => handleSubmit(e, store)}
+        >
+          <input
+            css={{
+              borderRadius: "20px",
+              boxShadow: "inset 1px 1px 2px 0 rgba(0,0,0,0.1)",
+              width: "100%",
+              padding: "10px",
+              outline: 0,
+            }}
+            type="text"
+            id={"searchTerm"}
+            className="form-control"
+            placeholder={"Search English"}
+            ref={(node) => {
+              searchTerm = node;
+            }}
+          />
+
+          <button
+            type="submit"
+            value="Submit"
+            css={{
+              width: "40px",
+              height: "40px",
+              verticalAlign: 0,
+              borderRadius: "50%",
+              backgroundColor: "#FCA311",
+              color: "#000000",
+              marginLeft: "10px",
+              borderColor: "transparent",
+              "&:hover": {
+                cursor: "pointer",
+              },
+              "&:focus": {
+                outline: "none",
+              },
+            }}
+          >
+            <i class="fas fa-search"></i>{" "}
+          </button>
         </form>
       )}
     </StoreContext.Consumer>
